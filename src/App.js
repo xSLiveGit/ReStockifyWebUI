92ec646b47dba9e8c934989c7f04dffb4c6601f4
Sergiu MARIES <UnknownEmail>
2023-10-17 21:12:49 +0300

    dummy implementation with rendering the json for the PEP ticker

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
-export default function MyApp() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+function App() {
+  const [data, setData] = useState(null);
 
-  function handleClick() {
-    setCount1(count1 + 1);
-    setCount2(count2 + 2);
-  }
+  const fetchData = async (ticker) => {
+    try {
+      // Define the API endpoint URL
+      const apiUrl = `http://127.0.0.1:8080/item/${ticker}`;
+  
+      // Make a GET request using Axios
+      const response = await axios.get(apiUrl);
+      console.log("response", response);
+  
+      // Access the response data
+      setData(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData("PEP");
+  }, []);
 
   return (
     <div>
-      <h1>Counters that update separately</h1>
-      <MyButton count1={count1} count2={count2} onClick={handleClick} />
-      <MyButton count1={count1} count2={count2} onClick={handleClick} />
+      {data ? (
+        <pre>{JSON.stringify(data, null, 4)}</pre>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   );
 }
 
-function MyButton({count1, count2, onClick}) {
+function MyButton({ count1, count2, onClick }) {
   return (
     <button onClick={onClick}>
-      Clicked {count1} and {count2} times 
+      Clicked {count1} and {count2} times
     </button>
   );
 }
+
+export default App;
